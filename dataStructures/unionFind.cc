2425c9c87bee1c332d71461b43b953a612a2d66f
Björn Schumacher <info@i3j03rn.net>
2020-02-10 22:10:52 +0100

    added union find

---
+++ dataStructures/unionFind.cc
@@ -0,0 +1,31 @@
+class unionFind {
+ public:
+  unionFind(int size)
+      : _size(size), data(size, -1) {}
+  int find(int toFind) {
+    int root = toFind;
+    while (data[root] >= 0)
+      root = data[root];
+    while (toFind != root) {
+      int tmp = data[toFind];
+      data[toFind] = root;
+      toFind = tmp;
+    }
+    return root;
+  }
+  void join(int a, int b) {
+    if (data[a] < data[b]) {
+      data[a] += data[b];
+      data[b] = a;
+    } else {
+      data[b] += data[a];
+      data[a] = b;
+    }
+    _size--;
+  }
+  int size() { return _size; }
+
+ private:
+  vi data;
+  int _size;
+};
