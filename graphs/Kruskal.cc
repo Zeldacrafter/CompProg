71ff7f67cb52be7648c5ea9fcc6f946b16392142
Björn Schumacher <info@i3j03rn.net>
2020-02-11 13:14:06 +0100

    added Kruskal for MST

---
+++ graphs/Kruskal.cc
@@ -0,0 +1,22 @@
+// node a, node b, distance from a to b
+typedef tuple<int, int, double> iid;
+class unionFind;
+int main() {
+  vector<iid> edges;
+  sort(ALL(edges),
+       [](iid& a, iid& b) -> bool {
+         return get<2>(a) < get<2>(b);
+       });
+  unionFind uf(V);
+  double weigth = 0;
+  vi mst;
+  for (int i = 0; uf.size() != 1; ++i) {
+    int a = uf.find(get<0>(edges[i]));
+    int b = uf.find(get<1>(edges[i]));
+    if (a != b) {
+      uf.join(a, b);
+      mst.pb(i);
+      weigth += get<2>(edges[i]);
+    }
+  }
+}
