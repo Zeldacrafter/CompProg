bf25587174b08b583af805f47d785639a1fc64ef
Björn Schumacher <info@i3j03rn.net>
2020-02-16 14:23:13 +0100

    added ternary Search

---
+++ utils/ternarySearch.cc
@@ -0,0 +1,16 @@
+#include "../template.cc"
+// f unimodal function on [l, r]
+double terSearch(double l, double r,
+                 function<double(double)> f,
+                 bool findMax = true) {
+  const double eps = 1e-9;
+  while (abs(r - l) > eps) {
+    double m1 = l + (r - l) / 3;
+    double m2 = r - (r - l) / 3;
+    if (findMax == (f(m1) < f(m2)))
+      l = m1;
+    else
+      r = m2;
+  }
+  return l;
+}
