c0844ce6e75cbaae345da03e729b6adcb2b4028c
Björn Schumacher <info@i3j03rn.net>
2020-02-16 23:57:27 +0100

    added bridges and articulation points

---
+++ graphs/articulationPoints.cc
@@ -0,0 +1,31 @@
+#include "../template.cc"
+vi dfsNum, low;
+int dfsCounter = 0;
+int artiDfs(int v, vvi& adj, vi& a,
+            int p = -1) {
+  dfsNum[v] = low[v] = dfsCounter++;
+  int children = 0;
+  bool aP = false;
+  for (int u : adj[v]) {
+    if (u == p) continue;
+    if (dfsNum[u] == -1) {
+      ckmin(low[v], artiDfs(u, adj, a, v));
+      if (low[u] >= dfsNum[v] && p != -1 &&
+          !aP) {
+        a.pb(v);
+        aP = true;
+      }
+      children++;
+    } else
+      ckmin(low[v], dfsNum[u]);
+  }
+  if (p == -1 && children > 1) a.pb(v);
+  return low[v];
+}
+void findArtiPoints(vvi& adj, vi& artiP) {
+  dfsNum.assign(SZ(adj), -1);
+  low.assign(SZ(adj), -1);
+  F0R (v, SZ(adj))
+    if (dfsNum[v] == -1)
+      artiDfs(v, adj, artiP);
+}
---
+++ graphs/bridges.cc
@@ -0,0 +1,23 @@
+#include "../template.cc"
+vi dfsNum, low;
+int dfsCounter = 0;
+int bridgeDfs(int v, vvi& adj, vii& b,
+              int p = -1) {
+  dfsNum[v] = low[v] = dfsCounter++;
+  for (int u : adj[v]) {
+    if (u == p) continue;
+    if (dfsNum[u] == -1) {
+      ckmin(low[v], bridgeDfs(u, adj, b, v));
+      if (low[u] > dfsNum[v]) b.eb(v, u);
+    } else
+      ckmin(low[v], dfsNum[u]);
+  }
+  return low[v];
+}
+void findBridges(vvi& adj, vii& bridges) {
+  dfsNum.assign(SZ(adj), -1);
+  low.assign(SZ(adj), -1);
+  F0R (v, SZ(adj))
+    if (dfsNum[v] == -1)
+      bridgeDfs(v, adj, bridges);
+}
