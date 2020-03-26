0e438be5b0b7d26befed25feba3880fbdefb1ad9
AlexP <alex.pfanne1@gmail.com>
2020-03-26 12:57:26 +0100

    Aho Corasick String Matching

---
+++ strings/aho_corasick.cc
@@ -0,0 +1,44 @@
+#include "../template.cc"
+// First call insert() on all relevant t_i
+// then call buildAC()
+int nxt[MAXN][26], fail[MAXN], val[MAXN],
+    nodeCnt = 0;
+void insert(string& s, int v) {
+  int curr = 0;
+  for (char c : s) {
+    if (!nxt[curr][c - 'a'])
+      nxt[curr][c - 'a'] = ++nodeCnt;
+    curr = nxt[curr][c - 'a'];
+  }
+  val[curr] += v;
+}
+void buildAC() {
+  queue<int> q;
+  F0R (i, 26)
+    if (nxt[0][i]) q.push(nxt[0][i]);
+  while (!q.empty()) {
+    int curr = q.front(), q.pop();
+    F0R (i, 26) {
+      if (nxt[curr][i]) {
+        fail[nxt[curr][i]] =
+            nxt[fail[curr]][i];
+        val[nxt[curr][i]] +=
+            val[nxt[fail[curr]][i]];
+        q.push(nxt[curr][i]);
+      } else
+        nxt[curr][i] = nxt[fail[curr]][i];
+    }
+  }
+}
+int query(string& s) {
+  int curr = 0, res = 0;
+  for (char c : s) {
+    if (nxt[curr][c - 'a'])
+      curr = nxt[curr][c - 'a'];
+    else
+      while (curr && !nxt[curr][c - 'a'])
+        curr = fail[curr];
+    res += val[curr];
+  }
+  return res;
+}
