7dac7435504db55da55a88fb2d0c6270ccd23458
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-04-15 13:33:58 +0200

    Miller-Rabin is now deterministic

--- numberTheory/millerRabin.cc
+++ numberTheory/millerRabin.cc
@@ -1,27 +1,26 @@
-#include "../template.cc"
-bool checkComp(ll n, ll a, ll d, int s) {
-  ll x = modpow(a, d, n);
-  if (x == 1 || x == n - 1) return false;
-  FOR (r, 1, s) {
-    x = (__int128)x * x % n;
-    if (x == n - 1) return false;
+#include "../template.cc" // Taken from https://github.com/kth-competitive-programming/kactl/blob/master/content/number-theory/MillerRabin.h
+typedef long double ld;
+typedef unsigned long long ull;
+bool isPrime(ull n) {
+  if(n < 2 || n % 6 % 4 != 1) return (n | 1) == 1;
+  ull A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+  ull s = __builtin_ctzll(n - 1), d = n >> s;
+  for (ull a : A) {
+    ull p = modpow(a, d, n), i = s;
+    while (p != 1 && p != n - 1 && a % n && i--)
+      p = modmul(p, p, n);
+    if (p != n - 1 && i != s) return false;
   }
   return true;
-};
+}
 
-bool isPrime(ll n, int iter = 10) {
-  if (n < 4) return n == 2 || n == 3;
-
-  int s = 0;
-  ll d = n - 1;
-  while (!(d & 1)) {
-    d >>= 1;
-    s++;
-  }
-
-  F0R (i, iter)
-    if (checkComp(n, 2 + rand() % (n - 3), d,
-                  s))
-      return false;
-  return true;
+ull modmul(ull a, ull b, ull M) {
+  ll ret = a * b - M * ull(ld(a) * ld(b) / ld(M));
+  return ret + M * (ret < 0) - M * (ret >= (ll)M);
+}
+ull modpow(ull b, ull e, ull mod) {
+  ull ans = 1;
+  for (; e; b = mod_mul(b, b, mod), e /= 2)
+    if (e & 1) ans = mod_mul(ans, b, mod);
+  return ans;
 }
