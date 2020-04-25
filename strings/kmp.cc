8bf02e2fb3beae2c1f7226ed827a70ce89a576e2
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-04-25 13:54:36 +0200

    added kmp linear pattern matching

---
+++ strings/kmp.cc
@@ -0,0 +1,23 @@
+#include "../template.cc"
+vi preprocess(string& s) {
+  vi fail(SZ(s) + 1);
+  fail[0] = -1;
+  for (int i = 0, j = -1; i < SZ(s);) {
+    while (j >= 0 && s[i] != s[j]) j = fail[j];
+    ++i, ++j;
+    fail[i] = j;
+  }
+  return fail;
+}
+vi match(string& text, string& pattern) {
+  vi matches, fail(preprocess(pattern));
+  for (int i = 0, j = 0; i < SZ(text);) {
+    while (j >= 0 && text[i] != pattern[j]) j = fail[j];
+    ++i, ++j;
+    if (j == SZ(pattern)) {
+      matches.pb(i - j);
+      j = fail[j];
+    }
+  }
+  return matches;
+}
