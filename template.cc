4bcfde53c52845064f49fa3e7b0ca9ec49ae8237
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-04-25 11:10:39 +0200

    removed unnecessary parentheses in template

--- template.cc
+++ template.cc
@@ -20,8 +20,8 @@ typedef vector<vii> vvii;
 #define ft front()
 #define bk back()
 #define endl '\n'
-#define FOR(a, b, c) for (auto(a) = (b); (a) < (c); ++(a))
-#define ROF(a, b, c) for (auto(a) = (b); (a) > (c); --(a))
+#define FOR(a, b, c) for (auto a = (b); (a) < (c); ++(a))
+#define ROF(a, b, c) for (auto a = (b); (a) > (c); --(a))
 #define F0R(a, b) FOR ((a), 0, (b))
 #define R0F(a, b) ROF ((a), (b), -1)
 #define CEIL(a, b) ((a) + (b)-1) / (b)
