088c68e94af5a02ac987c0378b5734b1678fee2e
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-04-25 14:55:08 +0200

    added the prefix function for strings

---
+++ strings/prefixFunction.cc
@@ -0,0 +1,12 @@
+#include "../template.cc"
+vi prefixFunction(string& s) {
+  vi prefix(SZ(s));
+  FOR (i, 1, SZ(s)) {
+    int j = prefix[i - 1];
+    while (j > 0 && s[i] != s[j])
+      j = prefix[j - 1];
+    if (s[i] == s[j]) ++j;
+    prefix[i] = j;
+  }
+  return prefix;
+}
