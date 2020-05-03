d1f384437f9a968d7297b1e035bb9c5379860026
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-05-03 02:37:28 +0200

    nice

--- utils/ordered_set.cc
+++ utils/ordered_set.cc
@@ -3,8 +3,8 @@
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 template <typename T>
-struct oset : public tree<T, null_type, less<T>, rb_tree_tag,
-                          tree_order_statistics_node_update> {};
+using oset = tree<T, null_type, less<T>, rb_tree_tag,
+                  tree_order_statistics_node_update>;
 // same operations as set w/ extra options:
 // - oset.find_by_order(int k) returns iterator to the k-th smallest
 //   entry (0 indexed)
