4e48a31061b32e982ac471d19f94113aa1c3e78a
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-05-04 17:03:00 +0200

    extended the mod struct

--- utils/mod.cc
+++ utils/mod.cc
@@ -3,11 +3,36 @@ struct mod {
   static const ll p = 1e9 + 7;
   ll val;
   mod() : val(0) {}
-  mod(ll val) : val(val % p) {}
-  mod operator+(mod o) { return mod(val + o.val); }
-  mod operator-(mod o) { return mod(val - o.val + p); }
-  mod operator*(mod o) { return mod(val * o.val); }
-  mod operator/(mod o) { return mod(*this * pow(o, p - 2)); }
+  mod(ll val) : val(val % p) {
+    if (this->val < 0) this->val += p;
+  }
+  mod& operator+=(mod o) {
+    val = (val + o.val) % p;
+    return *this;
+  }
+  mod& operator-=(mod o) {
+    val = (val - o.val + p) % p;
+    return *this;
+  }
+  mod& operator*=(mod o) {
+    val = val * o.val % p;
+    return *this;
+  }
+  mod& operator/=(mod o) { return (*this) *= pow(o, p - 2); }
+  mod& operator++() { return (*this) += 1; }
+  mod operator++(int) {
+    ++(*this);
+    return (*this) - 1;
+  }
+  mod& operator--() { return (*this) -= 1; }
+  mod operator--(int) {
+    --(*this);
+    return (*this) + 1;
+  }
+  mod friend operator+(mod a, mod b) { return a += b; }
+  mod friend operator-(mod a, mod b) { return a -= b; }
+  mod friend operator*(mod a, mod b) { return a *= b; }
+  mod friend operator/(mod a, mod b) { return a /= b; }
   static mod pow(mod a, mod b) {
     mod res = 1;
     for (; b.val; b.val >>= 1, a = a * a)
@@ -15,12 +40,10 @@ struct mod {
     return res;
   }
   friend istream& operator>>(istream& i, mod& m) {
-      i >> m.val;
-      m.val %= mod::p;
-      if (m.val < 0) m.val += mod::p;
-      return i;
+    i >> m.val;
+    m.val %= mod::p;
+    if (m.val < 0) m.val += mod::p;
+    return i;
   }
-  friend ostream& operator<<(ostream& o, mod m) {
-    return o << m.val;
-  }
+  friend ostream& operator<<(ostream& o, mod m) { return o << m.val; }
 };
