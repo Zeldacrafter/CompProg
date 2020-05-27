1365b9bd1fa0853cb46892c2f1b988cdcc97880a
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-05-27 16:42:52 +0200

    Toposort Kahn with vector instead of queue to have results directly

--- graphs/topoSortKahn.cc
+++ graphs/topoSortKahn.cc
@@ -3,14 +3,11 @@ int N;
 vvi adj(N);
 vi in(N);  // in degree for every node
 int main() {
-  queue<int> q;
+  vi q; // Result saved in q
   F0R (i, N)
-    if (in[i] == 0) q.push(i);
-  while (!q.empty()) {
-    int curr = q.front();
-    q.pop();
-    cout << curr << '\n';
-    for (int v : adj[curr])
-      if (--in[v] == 0) q.push(v);
-  }
+    if (!in[i]) q.pb(i);
+  F0R (i, SZ(q))
+    for (int v : adj[q[i]])
+      if (--in[v] == 0) 
+          q.pb(v);
 }
