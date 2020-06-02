b6f208757bc24846c2b25624f2dbd587850dcc7e
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-06-02 14:48:17 +0200

    add dinic

---
+++ graphs/dinic.cc
@@ -0,0 +1,46 @@
+
+#include "flowedge.cc"
+vi dist, ptr;
+const ll inf = 1e16;
+bool dinic_bfs(int s, int t) {
+  dist.assign(SZ(adj), SZ(adj));
+  queue<int> q;
+  q.push(s);
+  dist[s] = 0;
+  while (SZ(q)) {
+    int v = q.front(); q.pop();
+    for (int i : adj[v]) {
+      edge& e = edges[i];
+      if (dist[e.other(v)] == SZ(adj) && e.flow(v) < e.capacity(v)) {
+        dist[e.other(v)] = dist[v] + 1;
+        q.push(e.other(v));
+      }
+    }
+  }
+  return dist[t] < SZ(adj);
+}
+ll dinic_dfs(int v, int t, ll available) {
+  if (v == t || !available) return available;
+  ll pushed = 0;
+  for (; ptr[v] < SZ(adj[v]); ++ptr[v]) {
+    edge& e = edges[adj[v][ptr[v]]];
+    if (dist[v] + 1 != dist[e.other(v)] || e.flow(v) >= e.capacity(v))
+      continue;
+    ll wasPushed =
+        dinic_dfs(e.other(v), t,
+                  min(available - pushed, e.capacity(v) - e.flow(v)));
+    pushed += wasPushed;
+    e.adjust(v, wasPushed);
+    if (pushed == available) return pushed;
+  }
+  return pushed;
+}
+ll maxflow(int s, int t) {
+  dist.assign(SZ(adj), 0);
+  ll f = 0;
+  while (1) {
+    if (!dinic_bfs(s, t)) return f;
+    ptr.assign(SZ(adj), 0);
+    f += dinic_dfs(s, t, inf);
+  }
+}
