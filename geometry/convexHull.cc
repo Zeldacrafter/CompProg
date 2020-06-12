6f91eb59169873fb5728d072ac0bcc5ae0fd9de8
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-06-12 03:08:49 +0200

    Convex ist das Leben lustig! *_*

--- geometry/convexHull.cc
+++ geometry/convexHull.cc
@@ -1,35 +1,35 @@
-#include "geometry.cc"
-void convexHull(vector<ci>& pts) {
-  // Assume that pts[0] != pts[SZ(pts) - 1]
-  if (!SZ(pts)) return;
-  sort(ALL(pts), [](const ci& a, const ci& b) {
-    if (real(a) != real(b))
-      return real(a) < real(b);
-    else
-      return imag(a) < imag(b);
-  });
-
-  ci fst = pts[0];
-  ci lst = pts[SZ(pts) - 1];
-  vector<ci> up, down;
-  up.pb(fst);
-  down.pb(fst);
-
-  for (ci p : pts) {
-    if (p == lst || cw(fst, p, lst)) {
-      while (SZ(up) >= 2 && !cw(up[SZ(up) - 2], up[SZ(up) - 1], p))
+#include "../template.cc"
+#define xx real()
+#define yy imag()
+using pt = complex<double>;
+double dir(pt a, pt b, pt c) {
+  pt ab = b - a, ac = c - a;
+  return ab.xx * ac.yy - ab.yy * ac.xx;
+}
+bool cw(pt a, pt b, pt c) { return dir(a, b, c) < 0; }
+bool ccw(pt a, pt b, pt c) { return dir(a, b, c) > 0; }
+// careful with inputs for n < 2
+vector<pt> convexHull(vector<pt>& pts) {
+  int n = SZ(pts);
+  sort(ALL(pts),
+       [](pt a, pt b) { return mp(a.xx, a.yy) < mp(b.xx, b.yy); });
+  vector<pt> up, down;
+  up.eb(pts[0]); down.eb(pts[0]);
+  FOR (i, 1, n) {
+    if (i == n - 1 || ccw(pts[0], pts[n - 1], pts[i])) {
+      while (SZ(up) > 1 &&
+             ccw(up[SZ(up) - 2], up[SZ(up) - 1], pts[i]))
         up.pop_back();
-      up.pb(p);
+      up.eb(pts[i]);
     }
-    if (p == lst || ccw(fst, p, lst)) {
-      while (SZ(down) >= 2 &&
-             !ccw(down[SZ(down) - 2], down[SZ(down) - 1], p))
+    if (i == n - 1 || cw(pts[0], pts[n - 1], pts[i])) {
+      while (SZ(down) > 1 &&
+             cw(down[SZ(down) - 2], down[SZ(down) - 1], pts[i]))
         down.pop_back();
-      down.pb(p);
+      down.eb(pts[i]);
     }
   }
-
-  pts = vector<ci>(down);
-  R0F (i, SZ(up) - 2)
-    pts.pb(up[i]);
+  vector<pt> ans(up);
+  ans.insert(ans.end(), 1 + RALL(down));
+  return ans;
 }
