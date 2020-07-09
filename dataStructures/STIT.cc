4567a5124e404d9a29ec1c0411fed822883612ca
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-07-09 14:57:02 +0200

    stit

---
+++ dataStructures/STIT.cc
@@ -0,0 +1,29 @@
+#include "../template.cc"
+struct ST {
+  using T = int;
+  const T unit = 0;
+  T merge(T l, T r) { return l + r; };
+  int n;
+  vector<T> data;
+  ST(int n) : n(n), data(2 * n) {}
+  ST(vector<T>& v) : n(SZ(v)), data(2 * SZ(v)) {
+    copy(ALL(v), data.begin() + SZ(v));
+    build();
+  }
+  void build() {
+    for (int i = n - 1; i > 0; --i)
+      data[i] = merge(data[i << 1], data[i << 1 | 1]);
+  }
+  T query(int l, int r) {
+    T li = unit, ri = unit;
+    for (l += n, r += n; l < r; r >>= 1, l >>= 1) {
+      if (l & 1) li = merge(li, data[l++]);
+      if (r & 1) ri = merge(data[--r], ri);
+    }
+    return merge(li, ri);
+  }
+  void update(int i, T val) {
+    for (data[i += n] = val; i > 1; i >>= 1)
+      data[i >> 1] = merge(data[i & ~1], data[i | 1]);
+  }
+};
