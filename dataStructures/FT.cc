a34f59127a26bdae298e66ded2062da548128d31
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-08-23 10:48:57 +0200

    FTs now easier modifiable

--- dataStructures/FT.cc
+++ dataStructures/FT.cc
@@ -1,26 +1,26 @@
 #include "../template.cc"
 struct FT {
-  vi A;
+  using T = int;
+  vector<T> A;
   int n;
   FT(int n) : A(n + 1, 0), n(n) {}
   int inline LSOne(int i) { return i & (-i); }
-  int query(int i) {
-    int sum = 0;
+  T query(int i) {
+    T sum = 0;
     for (; i; i -= LSOne(i)) sum += A[i];
     return sum;
   }
-  int query(int i, int j) { return query(j) - query(i - 1); }
-  void update(int i, int add) {
+  T query(int i, int j) { return query(j) - query(i - 1); }
+  void update(int i, T add) {
     for (; i <= n; i += LSOne(i)) A[i] += add;
   }
   // lb assumes query(i, i) >= 0 forall i in [1, n]
   // returns min p >= 1, so that [1, p] >= sum
   // if [1, n] < sum, return n + 1
-  int lb(int sum) {
+  int lb(T sum) {
     int pos = 0;
     for (int pw = 1 << 25; pw; pw >>= 1)
       if (pos + pw <= n && sum > A[pos | pw]) sum -= A[pos |= pw];
-    if (sum) ++pos;
-    return pos;
+    return pos + !!sum;
   }
 };
--- dataStructures/FT2D.cc
+++ dataStructures/FT2D.cc
@@ -1,33 +1,35 @@
 #include "../template.cc"
 struct FT2D {
   struct FT {
-    vi A;
+    using T = int;
+    vector<T> A;
     int n;
     FT(int n) : A(n + 1, 0), n(n) {}
     int inline LSOne(int i) { return i & (-i); }
-    int query(int i) {
-      int sum = 0;
+    T query(int i) {
+      T sum = 0;
       for (; i; i -= LSOne(i)) sum += A[i];
       return sum;
     }
-    int query(int i, int j) { return query(j) - query(i - 1); }
+    T query(int i, int j) { return query(j) - query(i - 1); }
     void update(int i, int add) {
       for (; i <= n; i += LSOne(i)) A[i] += add;
     }
   };
+  using T = FT::T;
   vector<FT> fts;
   int n;
   FT2D(int n, int m) : fts(n + 1, FT(m)), n(n) {};
   int inline LSOne(int i) { return i & (-i); }
-  int query(int i, int j1, int j2) {
-    int sum = 0;
+  T query(int i, int j1, int j2) {
+    T sum = 0;
     for (; i; i -= LSOne(i)) sum += fts[i].query(j1, j2);
     return sum;
   }
-  int query(int i1, int i2, int j1, int j2) {
+  T query(int i1, int i2, int j1, int j2) {
     return query(i2, j1, j2) - query(i1 - 1, j1, j2);
   }
-  void update(int i, int j, int add) {
+  void update(int i, int j, T add) {
     for (; i <= n; i += LSOne(i)) fts[i].update(j, add);
   }
 };
