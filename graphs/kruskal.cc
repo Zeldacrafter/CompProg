3039a40cad7419906ae803a8c966054ca933022d
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-10-02 01:45:53 +0200

    Kruskal: Indicate if no MST found + no RE

--- graphs/kruskal.cc
+++ graphs/kruskal.cc
@@ -1,13 +1,12 @@
 #include "../dataStructures/DSU.cc"
-// re if graph is not connected
 vector<tuple<double, int, int>> edges;
-int V;
-pair<double, vi> kruskal() {
+int V; // bool indicates if MST could be found / graph is connected
+tuple<bool, double, vi> kruskal() {
   sort(ALL(edges)); DSU dsu(V); vi mst;
   double weigth = 0;
-  for (int i = 0; SZ(dsu) > 1; ++i) {
+  for (int i = 0; SZ(dsu) > 1 && i < SZ(edges); ++i) {
     auto [d, a, b] = edges[i];
     if (dsu.join(a, b)) mst.pb(i), weigth += d;
   }
-  return mp(weigth, mst);
+  return mt(SZ(dsu) == 1, weigth, mst);
 }
