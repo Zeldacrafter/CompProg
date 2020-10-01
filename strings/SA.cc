1ccccb7c94f76bfd2f24cef965b54ab6eb4913db
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-10-01 22:19:08 +0200

    suffix array now beautiful

--- strings/SA.cc
+++ strings/SA.cc
@@ -2,17 +2,12 @@
 vii SA_ns, SA_bs;
 template<int B = 'a', int S = 26, int N = 3>
 struct SA {
-  template<typename T>
-  static inline int cmp(T a, T b) {
-    return (a > b) - (a < b);
-  }
-  template<typename T>
-  static inline T getOr(const vector<T>& v, const int idx, T alternative) {
+  static inline int cmp(int a, int b) { return (a > b) - (a < b); }
+  static inline int getOr(const vi& v, int idx, int alternative = -1) {
     return idx < SZ(v) ? v[idx] : alternative;
   }
-  template<int I>
-  static void rsort(vi& v, function<int(int, int)> bf) {
-    for (int d = I - 1; ~d; --d) {
+  static void rsort(vi& v, int iters, function<int(int, int)> bf) {
+    for (int d = iters - 1; ~d; --d) {
       int mx = 0;
       F0R (i, SZ(v)) {
         int b = bf(v[i], d);
@@ -20,103 +15,72 @@ struct SA {
           fill(SA_bs.begin() + mx, SA_bs.begin() + b + 1, mp(-1, -1));
           mx = b + 1;
         }
-        if (SA_bs[b].fi == -1) {
+        if (SA_bs[b].fi == -1)
           SA_bs[b] = mp(i, i);
-        }
         SA_ns[SA_bs[b].se].se = i;
         SA_ns[i] = mp(v[i], -1);
         SA_bs[b].se = i;
       }
-      for (int j = 0, i = 0; i < mx; ++i) {
-        for (int it = SA_bs[i].fi; ~it; it = SA_ns[it].se) {
+      int j = 0;
+      F0R (i, mx)
+        for (int it = SA_bs[i].fi; ~it; it = SA_ns[it].se)
           v[j++] = SA_ns[it].fi;
-        }
-      }
     }
   }
   vi sa;
-  vi build(const vi& pref) {
-    vi one; one.reserve(SZ(pref) + 1);
-    for (int i = 0; i < SZ(pref); i += N) {
-      one.pb(i);
-    }
-    vi more; more.reserve(SZ(pref) - SZ(one));
-    FOR (j, 1, N) {
-      for (int i = j; i < SZ(pref); i += N) {
+  vi build(const vi& prefRank) {
+    vi one; one.reserve((SZ(prefRank) + N - 1)/N);
+    for (int i = 0; i < SZ(prefRank); i += N) one.pb(i);
+    vi more; more.reserve(SZ(prefRank) - SZ(one));
+    FOR (j, 1, N)
+      for (int i = j; i < SZ(prefRank); i += N)
         more.pb(i);
-      }
-    }
-    vi pos(SZ(pref), -1); 
-    F0R (i, SZ(more)) {
-      pos[more[i]] = i;
-    }
-    rsort<N>(more, [&](int i, int it) {
-      return getOr(pref, i + it, -1) + 1;
-    });
+    vi pos(SZ(prefRank), -1); 
+    F0R (i, SZ(more)) pos[more[i]] = i;
+    rsort(more, N, [&](int i, int it) { return getOr(prefRank, i + it) + 1; });
     vi ra(SZ(more));
-    bool rec = false;
-    for (int r = 0, i = 0; i < SZ(more); ++i) {
+    int r = 0;
+    F0R (i, SZ(more)) {
       ra[pos[more[i]]] = r;
-      if (i && ra[pos[more[i - 1]]] == r) rec = true;
-      if (i + 1 < SZ(more)) {
-        F0R (j, N) {
-          if (getOr(pref, more[i] + j, -1) != getOr(pref, more[i + 1] + j, -1)) {
-            ++r; break;
+      if (i + 1 < SZ(more))
+        F0R (j, N)
+          if (getOr(prefRank, more[i] + j) != getOr(prefRank, more[i + 1] + j)) {
+            r++; break;
           }
-        }
-      }
     }
-    if (rec) {
+    if (r + 1 < SZ(more)) {
       vi got = build(ra);
-      F0R (i, SZ(got)) {
-        ra[got[i]] = i;
-      }
-      F0R (i, SZ(pos)) {
-        if (~pos[i]) {
+      F0R (i, SZ(got)) ra[got[i]] = i;
+      F0R (i, SZ(pos)) 
+        if (~pos[i]) 
           more[ra[pos[i]]] = i;
-        }
-      }
     }
-    rsort<2>(one, [&](int i, int it) {
-      if (it) return getOr(ra, getOr(pos, i + 1, SZ(ra)), -1) + 1;
-      return pref[i];
+    rsort(one, 2, [&](int i, int it) {
+      return it ? getOr(ra, getOr(pos, i + 1, SZ(ra))) + 1 : prefRank[i];
     });
-    vi res; res.reserve(SZ(pref));
-    for (int o = 0, m = 0; o < SZ(one) || m < SZ(more);) {
-      if (o == SZ(one))  {
-        res.pb(more[m++]);
-      } else if (m == SZ(more)) {
-        res.pb(one[o++]);
-      } else {
-        int c = 0;
-        for (int k = 0; !c; ++k) {
-          int a = one[o] + k, b = more[m] + k;
-          if (a % N == 0 || b % N == 0) {
-            c = cmp(getOr(pref, a, -1), getOr(pref, b, -1));
-          } else {
-            c = cmp(getOr(ra, getOr(pos, a, -1), -1), getOr(ra, getOr(pos, b, -1), -1));
-          }
-        }
-        if (c < 0) {
-          res.pb(one[o++]);
-        } else {
-          res.pb(more[m++]);
-        }
+    vi res; res.reserve(SZ(prefRank));
+
+    int o = 0, m = 0;
+    while (o < SZ(one) && m < SZ(more)) {
+      int c = 0;
+      for (int k = 0; !c; ++k) {
+        int a = one[o] + k, b = more[m] + k;
+        c = (a % N && b % N) 
+            ? cmp(getOr(ra, getOr(pos, a, SZ(ra))), getOr(ra, getOr(pos, b, SZ(ra)))) 
+            : cmp(getOr(prefRank, a), getOr(prefRank, b));
       }
+      res.pb(c < 0 ? one[o++] : more[m++]);
     }
+    while (o == SZ(one) && m < SZ(more)) res.pb(more[m++]);
+    while (m == SZ(more) && o < SZ(one)) res.pb(one[o++]);
+
     return res;
   }
   SA(const string& s) {
     SA_bs.resize(max(S, SZ(s)) + 1);
     SA_ns.resize(max(S, SZ(s)) + 1);
-    vi v(SZ(s) + 1);
-    iota(ALL(v), 0);
-    vi ra(SZ(v));
-    F0R (i, SZ(s)) {
-      ra[i] = s[i] - B + 1;
-    }
-    ra[SZ(s)] = 0;
+    vi ra(SZ(s) + 1);
+    F0R (i, SZ(s)) ra[i] = s[i] - B + 1;
     sa = build(ra);
   }
 };
-
