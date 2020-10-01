16c913edd9ca82e5bc96e1d44a4cddfa18a1bdbb
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-10-01 15:21:50 +0200

    added const

--- geometry/polygon.cc
+++ geometry/polygon.cc
@@ -4,7 +4,7 @@ bool inTriangle(pt a, pt b, pt c, pt p) {
     abs(-abs(dir(a, b, c)) + abs(dir(a, b, p))
         + abs(dir(a, p, c)) + abs(dir(p, b, c))) < EPS;
 }
-bool inPolygon(vector<pt>& poly, pt p) {
+bool inPolygon(const vector<pt>& poly, pt p) {
   int l = 1, r = SZ(poly) - 2;
   while (l < r) {
     int mid = (l + r) / 2;
@@ -16,13 +16,13 @@ bool inPolygon(vector<pt>& poly, pt p) {
   }
   return inTriangle(poly[0], poly[l], poly[l - 1], p);
 }
-double area(vector<pt>& p) {
+double area(const vector<pt>& p) {
   double res = 0.0;
   F0R (i, SZ(p))
     res += cross(p[i], p[(i + 1) % SZ(p)]);
   return abs(res) / 2;
 }
-bool isConvex(vector<pt> p) {
+bool isConvex(const vector<pt>& p) {
   if (SZ(p) < 3) return false;
   bool isLeft = ccw(p[0], p[1], p[2]), convex = true;
   F0R (i, SZ(p))
