88df6c84f2cb7b7fd8c3ec05725f3e227cb0e241
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-10-02 12:25:42 +0200

    some rough drafts for optimization ideas [WIP]

--- strings/SA.cc
+++ strings/SA.cc
@@ -2,33 +2,25 @@
 vii SA_ns, SA_bs;
 template<int B = 'a', int S = 26, int N = 3>
 struct SA {
-  static inline int cmp(int a, int b) { return (a > b) - (a < b); }
-  static inline int getOr(const vi& v, int idx, int alternative = -1) {
-    return idx < SZ(v) ? v[idx] : alternative;
-  }
-  static void rsort(vi& v, int iters, function<int(int, int)> bf) {
-    for (int d = iters - 1; ~d; --d) {
-      int mx = 0;
-      F0R (i, SZ(v)) {
-        int b = bf(v[i], d);
-        if (b >= mx) {
-          fill(SA_bs.begin() + mx, SA_bs.begin() + b + 1, mp(-1, -1));
-          mx = b + 1;
-        }
-        if (SA_bs[b].fi == -1)
-          SA_bs[b] = mp(i, i);
-        SA_ns[SA_bs[b].se].se = i;
-        SA_ns[i] = mp(v[i], -1);
-        SA_bs[b].se = i;
-      }
-      int j = 0;
-      F0R (i, mx)
-        for (int it = SA_bs[i].fi; ~it; it = SA_ns[it].se)
-          v[j++] = SA_ns[it].fi;
-    }
-  }
   vi sa;
+  SA(const string& s) {
+    SA_bs.resize(max(S, SZ(s)) + 1);
+    SA_ns.resize(max(S, SZ(s)) + 1);
+    vi ra(SZ(s) + 1);
+    F0R (i, SZ(s)) ra[i] = s[i] - B + 1;
+    sa = build(ra);
+  }
   vi build(const vi& prefRank) {
+    // optimization ideas:
+    // - consolidate one and more into one array and work with iterators
+    //   - further can the consolidated array be used as the result
+    //     array with (semi-)inplace-merging.  Approaches:
+    //      1. allocate extra SZ(prefRank) / N to store the one part.
+    //         This gives enough space to merge inplace
+    // - get rid of the pos array.  Ideas:
+    //   1. use idx calcultion:
+    //      a. more[i] -> offset[more[i] % N] + more[i] / N;
+    //      b. pos[i] -> ...
     vi one; one.reserve((SZ(prefRank) + N - 1)/N);
     for (int i = 0; i < SZ(prefRank); i += N) one.pb(i);
     vi more; more.reserve(SZ(prefRank) - SZ(one));
@@ -71,16 +63,34 @@ struct SA {
       }
       res.pb(c < 0 ? one[o++] : more[m++]);
     }
-    while (o == SZ(one) && m < SZ(more)) res.pb(more[m++]);
-    while (m == SZ(more) && o < SZ(one)) res.pb(one[o++]);
+    while (m < SZ(more)) res.pb(more[m++]);
+    while (o < SZ(one)) res.pb(one[o++]);
 
     return res;
   }
-  SA(const string& s) {
-    SA_bs.resize(max(S, SZ(s)) + 1);
-    SA_ns.resize(max(S, SZ(s)) + 1);
-    vi ra(SZ(s) + 1);
-    F0R (i, SZ(s)) ra[i] = s[i] - B + 1;
-    sa = build(ra);
+  static inline int cmp(int a, int b) { return (a > b) - (a < b); }
+  static inline int getOr(const vi& v, int idx, int alternative = -1) {
+    return idx < SZ(v) ? v[idx] : alternative;
+  }
+  static void rsort(vi& v, int iters, function<int(int, int)> bf) {
+    for (int d = iters - 1; ~d; --d) {
+      int mx = 0;
+      F0R (i, SZ(v)) {
+        int b = bf(v[i], d);
+        if (b >= mx) {
+          fill(SA_bs.begin() + mx, SA_bs.begin() + b + 1, mp(-1, -1));
+          mx = b + 1;
+        }
+        if (SA_bs[b].fi == -1)
+          SA_bs[b] = mp(i, i);
+        SA_ns[SA_bs[b].se].se = i;
+        SA_ns[i] = mp(v[i], -1);
+        SA_bs[b].se = i;
+      }
+      int j = 0;
+      F0R (i, mx)
+        for (int it = SA_bs[i].fi; ~it; it = SA_ns[it].se)
+          v[j++] = SA_ns[it].fi;
+    }
   }
 };
