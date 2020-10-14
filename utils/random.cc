a3094dc995bcc813bec478dabd5fcd956ef8bcc3
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-10-15 00:26:22 +0200

    added utility classes

---
+++ utils/random.cc
@@ -0,0 +1,6 @@
+#include "../template.cc"
+mt19937 gen((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+// values from [a, b] use like Idst(gen);
+uniform_int_distribution<int> Idst(0, 10); 
+// values from [a, b) use like Idst(gen);
+uniform_real_distribution<double> Rdist(.0, 10.0);
