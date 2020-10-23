fbdc4015d28fe92f562f287807462e0234acb54a
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-10-23 21:53:49 +0200

    Test: Assiciative Array

---
+++ tests/associative_array.yosupo.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/associative_array"
+
+#include "../template.cc"
+
+int main() {
+  cin.tie(0);
+  ios_base::sync_with_stdio(0);
+
+  int q;
+  cin >> q;
+
+  map<ll, ll> arr;
+  while(q--) {
+    bool c;
+    cin >> c;
+    if(!c) {
+      ll k, v;
+      cin >> k >> v;
+      arr[k] = v;
+    } else {
+      ll k;
+      cin >> k;
+      cout << arr[k] << endl;
+    }
+  }
+}
+
+
