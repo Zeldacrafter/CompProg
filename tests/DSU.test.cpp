7a7a627f6f1e82dd9cfb716586778a3c2b3cbe54
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-10-23 21:04:01 +0200

    Create DSU.test.cpp

---
+++ tests/DSU.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include "../dataStructures/DSU.cc"
+
+int main() {
+  cin.tie(0);
+  ios_base::sync_with_stdio(0);
+  
+  int n, q;
+  cin >> n >> q;
+  
+  DSU dsu(n);
+  while(q--) {
+    int a, b ,c;
+    cin >> a >> b >> c;
+    if(a) {
+      dsu.join(b, c);
+    } else {
+      cout << dsu.sameSet(b, c) << endl;
+    }
+  }
+}
