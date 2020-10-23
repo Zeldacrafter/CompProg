7d6634a1856249ea02be315a01cf43468780d76e
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-10-23 22:18:24 +0200

    Test: Moving files

--- tests/yosupo/aplusb.test.cpp
+++ tests/yosupo/aplusb.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
 
-#include "../template.cc"
+#include "../../template.cc"
 
 int main() {
   cin.tie(0);
--- tests/yosupo/associative_array.test.cpp
+++ tests/yosupo/associative_array.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/associative_array"
 
-#include "../template.cc"
+#include "../../template.cc"
 
 int main() {
   cin.tie(0);
