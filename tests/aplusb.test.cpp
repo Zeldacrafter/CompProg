db231a56915cdf93875e5472e286b3d9ba2edb66
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-10-23 21:29:50 +0200

    aplusb.test.cpp

---
+++ tests/aplusb.test.cpp
@@ -0,0 +1,12 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include "../template.cc"
+
+int main() {
+  cin.tie(0);
+  ios_bas::sync_with_stdio(0);
+
+  ll a, b;
+  cin >> a >> b;
+  cout << a + b << endl;
+}
