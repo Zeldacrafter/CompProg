4d511432f0bef950004d8c20e114975d8570e687
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-10-23 20:27:59 +0200

    added Werror to compile script

--- buildall.sh
+++ buildall.sh
@@ -7,7 +7,7 @@ find . -name '*.cc' | while read line; do
     line=${line/.\//}
     echo "trying to compile $line"
     out="$tmp_dir/${line//\//-}"
-    g++ -c -Wall -Wextra -Wshadow -Wpedantic -std=c++17 -o $out $line
+    g++ -c -Wall -Wextra -Wshadow -Wpedantic -Werror -std=c++17 -o $out $line
 done
 
 rm -r $tmp_dir
