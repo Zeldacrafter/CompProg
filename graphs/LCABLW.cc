0e72b2a55eb7ea5e5a8e5f03cabad453aea434fd
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-10-26 01:01:02 +0100

    Reworked LCA, added variant without weights

--- graphs/LCABLW.cc
+++ graphs/LCABLW.cc
@@ -1,35 +1,41 @@
 #include "../template.cc"
 struct LCA {
   int n, logN, root;
-  vvi up;
+  vvi up, weight;
   vi h;
-  LCA(vvi& adj, int r = 0)
+  LCA(vvii& adj, int r = 0)
       : n{SZ(adj)}, logN{31 - __builtin_clz(n)},
         root{r}, up(n, vi(logN + 1, root)),
+        weight(n, vi(logN + 1, numeric_limits<int>::min())),
         h(n, -1) { 
     build(adj, root);
   }
-  void build(vvi& adj, int v) {
+  void build(vvii& adj, int v) {
     queue<int> q;
     q.push(v);
     h[v] = 0;
     while (SZ(q)) {
       int v = q.front();
       q.pop();
-      for (int u : adj[v])
+      for (auto [u, _] : adj[v])
         if (h[u] == -1) {
           h[u] = h[v] + 1;
           q.push(u);
         }
     }
     F0R (v, n)
-      for (int u : adj[v])
-        if (h[u] < h[v])
+      for (auto [u, w] : adj[v])
+        if (h[u] < h[v]) {
           up[v][0] = u;
+          weight[v][0] = w;
+        }
     FOR (exp, 1, logN + 1)
       F0R (v, n)
-        if (up[v][exp - 1] != -1)
+        if (up[v][exp - 1] != -1) {
           up[v][exp] = up[up[v][exp - 1]][exp - 1];
+          weight[v][exp] = max(weight[v][exp - 1], 
+                               weight[up[v][exp - 1]][exp - 1]);
+        }
   }
   int jumpUp(int v, int amt) {
     for (int i = 0; v != -1 && (1 << i) <= amt; ++i)
@@ -50,5 +56,21 @@ struct LCA {
       }
     }
     return up[v][0];
+  }
+  // returns max weight of an edge on the the path v -- u
+  int maxPath(int v, int u) {
+    int anc = lca(v, u);
+    return max(maxPathAnc(v, anc), maxPathAnc(u, anc));
+  }
+  // returns max weight of an edge on path from v to u (isAnc(u, v))
+  int maxPathAnc(int v, int u) {
+    int res = numeric_limits<int>::min();
+    for (int l = logN; ~l; --l) {
+      if (h[u] <= up[v][l]) {
+        ckmax(res, weight[v][l]);
+        v = up[v][l];
+      }
+    }
+    return res;
   }
 };
