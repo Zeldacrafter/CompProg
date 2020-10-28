bc9c136c88c203b61ab5998fdf043c8b3dea47a4
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-10-28 19:10:27 +0100

    :(

--- tests/hackerrank/aho_corasick.two_two.test.cpp
+++ tests/hackerrank/aho_corasick.two_two.test.cpp
@@ -7,14 +7,13 @@ int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
 
-    vector<pair<basic_string<char>, int>> strs;
+    vector<pair<string, int>> strs;
 
     bigint x = 1;
     F0R(i, 801) {
         strs.eb(to_string(x), 1);
         x *= 2;
     }
-    //for(auto [a, b] : strs) cout << a << endl;
 
     AHC<int, 10, char, '0'> ahc(0, strs);
 
@@ -23,6 +22,7 @@ int main() {
     while(t--) {
         string s;
         cin >> s;
-        cout << ahc.query(s) << endl;
+        cout << ahc.query(s);
+        if(t) cout << endl;
     }
 }
