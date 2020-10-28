001ae47e5176a450aa96da1cbc686f638ab32697
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-10-28 23:37:47 +0100

    updated lca to use sparsetable

--- code/graphs/LCA.cc
+++ code/graphs/LCA.cc
@@ -1,40 +1,26 @@
-#include "../template.hh"
+#include "../dataStructures/SPT.cc"
 struct LCA {
-  vi height, eulerTour, first;
-  vvi idx;
-  LCA(vvi& adj, int root = 0)
-      : height(SZ(adj), -1), first(SZ(adj), -1) {
-    dfs(adj, root);
-    build();
+  vi height, first;
+  SPT<int, function<int(int, int)>> spt;
+  LCA(vvi& adj, int root = 0) : height(SZ(adj), -1), first(SZ(adj), -1),
+      spt(2 * SZ(adj) - 1, [self = this](int a, int b) {
+        return self->height[a] < self->height[b] ? a : b;
+      }) {
+    int idx = 0;
+    dfs(adj, root, idx);
+    spt.build();
   }
-  void build() {
-    const int logn = ceil(log2l(SZ(eulerTour))) + 1;
-    idx.assign(SZ(eulerTour), vi(logn));
-    F0R (i, SZ(eulerTour))
-      idx[i][0] = eulerTour[i];
-    for (int j = 1; (1 << j) <= SZ(eulerTour); ++j)
-      for (int i = 0; i + (1 << j) <= SZ(eulerTour); ++i) {
-        int k = i + (1 << (j - 1));
-        idx[i][j] = height[idx[i][j - 1]] < height[idx[k][j - 1]]
-			? idx[i][j - 1] : idx[k][j - 1];
-      }
-  }
-  int rmq(int l, int r) {
-    int k = 31 - __builtin_clz(r - l + 1);
-    return height[idx[l][k]] < height[idx[r - (1 << k) + 1][k]]
-               ? idx[l][k] : idx[r - (1 << k) + 1][k];
-  }
-  void dfs(vvi& adj, int v = 0, int h = 0) {
-    eulerTour.pb(v);
-    first[v] = SZ(eulerTour) - 1;
+  void dfs(vvi& adj, int v, int& idx, int h = 0) {
+    first[v] = idx;
+    spt.d[0][idx++] = v;
     height[v] = h;
-    for (int u : adj[v])
-      if (first[u] == -1) {
-        dfs(adj, u, h + 1);
-        eulerTour.pb(v);
-      }
+    for (int u : adj[v]) if (first[u] == -1) {
+      dfs(adj, u, idx, h + 1);
+      spt.d[0][idx++] = v;
+    }
   }
   int lca(int a, int b) {
-    return rmq(min(first[a], first[b]), max(first[a], first[b]));
+    ii m = minmax(first[a], first[b]);
+    return spt.query(m.fi, m.se);
   }
 };
--- tests/yosupo/sparse_table.lca.test.cpp
+++ tests/yosupo/sparse_table.lca.test.cpp
@@ -6,21 +6,18 @@ int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
 
-    int n, q;
-    cin >> n >> q;
+    int n, q; cin >> n >> q;
 
     vvi adj(n);
-    F0R(i, n - 1) {
-        int u;
-        cin >> u;
-        adj[i + 1].pb(u);
-        adj[u].pb(i + 1);
+    FOR (i, 1, n) {
+        int u; cin >> u;
+        adj[i].pb(u);
+        adj[u].pb(i);
     }
-    
+
     LCA lca(adj);
     while(q--) {
-        int u, v;
-        cin >> u >> v;
+        int u, v; cin >> u >> v;
         cout << lca.lca(u, v) << endl;
     }
 }
