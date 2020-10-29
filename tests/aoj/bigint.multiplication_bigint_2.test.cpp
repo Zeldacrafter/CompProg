bde8f84a2f1a27fe8d6ac161c7481e39f5b44d72
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-10-29 22:02:54 +0100

    bigint multiplication test

---
+++ tests/aoj/bigint.multiplication_bigint_2.test.cpp
@@ -0,0 +1,12 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_2_F"
+
+#include "../../code/utils/bigint.cc"
+
+int main() {
+  cin.tie(0);
+  ios_base::sync_with_stdio(0);
+
+  bigint a, b;
+  cin >> a >> b;
+  cout << a * b << endl;
+}
