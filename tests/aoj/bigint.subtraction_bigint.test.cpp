b835a0c6959b3586caf043dcb37b1fe54bc8d965
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-10-29 21:57:58 +0100

    bigint subtraction test

---
+++ tests/aoj/bigint.subtraction_bigint.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_2_B"
+
+#include "../../code/utils/bigint.cc"
+
+int main() {
+  cin.tie(0);
+  ios_base::sync_with_stdio(0);
+
+  bigint a, b;
+  cin >> a >> b;
+  cout << a - b << endl;
+}
+
+
+
+
+
+
