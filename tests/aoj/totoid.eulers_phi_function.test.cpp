0b1b65501446f6e009e086093b1e8992d90042cd
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-10-29 22:43:21 +0100

    test for eulers totoid function

---
+++ tests/aoj/totoid.eulers_phi_function.test.cpp
@@ -0,0 +1,13 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_D"
+
+#include "../../code/math/totoid.cc"
+
+int main() {
+  cin.tie(0);
+  ios_base::sync_with_stdio(0);
+
+  ll n;
+  cin >> n;
+  cout << phi(n) << endl;
+}
+
