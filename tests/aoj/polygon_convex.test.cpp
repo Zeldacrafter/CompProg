b021a0de44c55f5eacdd0559b44b0b0d1a3a0f57
Alexander Pfanne <alex.pfanne1@gmail.com>
2020-10-30 09:34:44 +0100

    test: polygon isConvex

---
+++ tests/aoj/polygon_convex.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=CGL_3_B"
+
+#include "../../code/geometry/polygon.cc"
+
+int main() {
+  cin.tie(0);
+  ios_base::sync_with_stdio(0);
+
+  int n;
+  cin >> n;
+  vector<pt> pts(n);
+  F0R(i, n) {
+      int x, y;
+      cin >> x >> y;
+      pts[i] = pt(x, y);
+  }
+  cout << isConvex(pts) << endl;
+}
