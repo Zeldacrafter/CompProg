849bb25702e772fae51d7879bda522a818373912
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-11-16 16:00:31 +0100

    fixed indentation

--- code/utils/y_combinator.cc
+++ code/utils/y_combinator.cc
@@ -5,15 +5,15 @@
 //  2. http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
 template<typename F>
 struct y_combinator_result {
-    F f;
-    template<typename T>
-    explicit y_combinator_result(T&& fun): f(forward<T>(fun)) {}
-    template<typename... Args>
-    decltype(auto) operator()(Args&&... args) {
-        return f(ref(*this), forward<Args>(args)...);
-    }
+  F f;
+  template<typename T>
+  explicit y_combinator_result(T&& fun) : f(forward<T>(fun)) {}
+  template<typename... Args>
+  decltype(auto) operator()(Args&&... args) {
+    return f(ref(*this), forward<Args>(args)...);
+  }
 };
 template<typename F>
 decltype(auto) y_combinator(F&& f) {
-    return y_combinator_result<decay_t<F>>(forward<F>(f));
+  return y_combinator_result<decay_t<F>>(forward<F>(f));
 }
