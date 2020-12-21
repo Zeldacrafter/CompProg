cbe4d21ed75eab7a8bdc7a569ed77bc8c54b5099
Björn Schumacher <stu214530@mail.uni-kiel.de>
2020-12-21 13:08:28 +0100

    operator overloading for sets

---
+++ code/utils/setops.cc
@@ -0,0 +1,27 @@
+#include "../template.hh"
+template<typename T>
+set<T>& operator+=(set<T>& s, const T& e) {
+  return s.insert(e), s;
+}
+template<typename T>
+set<T>& operator-=(set<T>& s, const T& e) {
+  return s.erase(e), s;
+}
+template<typename T>
+set<T>& operator&=(set<T>& a, const set<T>& b) {
+  set<T> o;
+  set_intersection(ALL(a), ALL(b), inserter(o, o.begin()));
+  return a.swap(o), a;
+}
+template<typename T>
+set<T>& operator|=(set<T>& a, const set<T>& b) {
+  return a.insert(ALL(b)), a;
+}
+template<typename T>
+set<T> operator&(set<T> a, const set<T>& b) {
+  return a &= b;
+}
+template<typename T>
+set<T> operator|(set<T> a, const set<T>& b) {
+  return a |= b;
+}
