1005f8100d2e17232f905554c4c9633793c0e523
Björn Schumacher <stu214530@mail.uni-kiel.de>
2022-12-03 23:51:40 +0100

    more set ops

--- code/template_long.cc
+++ code/template_long.cc
@@ -285,12 +285,32 @@ set<T>& operator&=(set<T>& a, const set<T>& b) {
   return a.swap(o), a;
 }
 template<typename T>
+set<T>& operator^=(set<T>& a, const set<T>& b) {
+  set<T> o;
+  set_symmetric_difference(ALL(a), ALL(b), inserter(o, o.begin()));
+  return a.swap(o), a;
+}
+template<typename T>
+set<T>& operator/=(set<T>& a, const set<T>& b) {
+  set<T> o;
+  set_difference(ALL(a), ALL(b), inserter(o, o.begin()));
+  return a.swap(o), a;
+}
+template<typename T>
 set<T>& operator|=(set<T>& a, const set<T>& b) {
   return a.insert(ALL(b)), a;
 }
 template<typename T>
 set<T> operator&(set<T> a, const set<T>& b) {
   return a &= b;
+}
+template<typename T>
+set<T> operator^(set<T> a, const set<T>& b) {
+  return a ^= b;
+}
+template<typename T>
+set<T> operator/(set<T> a, const set<T>& b) {
+  return a /= b;
 }
 template<typename T>
 set<T> operator|(set<T> a, const set<T>& b) {
